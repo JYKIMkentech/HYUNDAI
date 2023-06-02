6c960a9cf19f5c36b1ae98c3684b36296840f8be
JYKIMkentech <129360396+JYKIMkentech@users.noreply.github.com>
2023-06-02 10:36:02 +0900

    Update JYK_dcir_resistance.m

--- JYK_dcir_resistance.m
+++ JYK_dcir_resistance.m
@@ -4,14 +4,14 @@ clc; clear; close all;
 data_folder = 'C:\Users\deu04\OneDrive\문서\MATLAB\dcir';
 save_path = data_folder;
 I_1C = 0.00382; %[A]
-id_cfa = 1; % 1 for cathode, 2 for fullcell , 3 for anode 
+id_cfa = 2; % 1 for cathode, 2 for fullcell , 3 for anode 
 
 % MAT 파일 가져오기
 slash = filesep;
 files = dir([data_folder slash '*.mat']);
 
 % 선택할 파일의 인덱스
-selected_file_index = 2; % 첫 번째 파일 선택
+selected_file_index = 1; % 첫 번째 파일 선택
 
 % 선택한 파일 load
 fullpath_now = [data_folder slash files(selected_file_index).name];
@@ -42,8 +42,8 @@ end
 
 for j = 1:length(data)
      %calculate capacities
-     data(j).Q = abs(trapz(data(j).t,data(j).I))/3600; %[Ah]
-     data(j).cumQ = abs(cumtrapz(data(j).t,data(j).I))/3600; %[Ah]
+     data(j).Q = trapz(data(j).t,data(j).I)/3600; %[Ah]
+     data(j).cumQ = cumtrapz(data(j).t,data(j).I)/3600; %[Ah]
      
 
      % data(j).cumQ = abs(cumtrapz(data(j).t,data(j).I))/3600; %[Ah]
@@ -68,9 +68,17 @@ end
 for i = 1 : length(data)
     if id_cfa == 1 || id_cfa == 2
         if id_cfa == 1
-            data(i).SOC = data(i).cumsumQ/total_QC; % Cathode
+            
+             data(i).SOC = data(i).cumsumQ/total_QC; % Cathode
+            
+
+          
         elseif id_cfa == 2
-            data(i).SOC = data(i).cumsumQ/total_QC; % FCC
+           
+             data(i).SOC = data(i).cumsumQ/total_QC; % FCC
+            
+            
+            
         end
         % 큰 I 가지는 index 추출
         BigI = [];
@@ -85,17 +93,17 @@ for i = 1 : length(data)
             BigIC = BigI(BigI < step_chg(end));
             BigID = BigI(BigI >= step_chg(end));
         end
-    elseif id_cfa == 3
+    elseif id_cfa == 3 % Anode
         BigI = [];
         for i = 1:length(data)
-            data(i).SOC = 1 - data(i).cumsumQ/total_QD;
+            data(i).SOC = 1 + data(i).cumsumQ/total_QD;
             if abs(data(i).I) > (1/3 * I_1C)
                BigI = [BigI , i];
                
             end
         end
         % BigI 계산
-        BigI = BigI;
+         BigI = BigI;
        
     else
         error('Invalid id_cfa value. Please choose 1 for cathode, 2 for FCC, or 3 for anode.');
@@ -171,29 +179,48 @@ end
 % 0.001s
 % CATHODE, FCC = BIGIC 데이터 확인
 % ANDOE = BIGI 데이터 확인
-SOC001s = [];
-R001s = [];
-SOC1s = [];
-R1s = [];
-SOC10s = [];
-R10s = [];
-SOC30s = [];
-R30s = [];
+SOC001sc = [];
+R001sc = [];
+SOC1sc = [];
+R1sc = [];
+SOC10sc = [];
+R10sc = [];
+SOC30sc = [];
+R30sc = [];
+SOC001sd = [];
+R001sd = [];
+SOC1sd = [];
+R1sd = [];
+SOC10sd = [];
+R10sd = [];
+SOC30sd = [];
+R30sd = [];
+
 
 if id_cfa == 1 || id_cfa == 2
     for i = 1:length(BigIC)
-        SOC001s = [SOC001s, data(BigIC(i)).SOC(2)];
-        R001s = [R001s, data(BigIC(i)).R001s];
-        SOC1s = [SOC1s, data(BigIC(i)).SOC(11)];
-        R1s = [R1s, data(BigIC(i)).R1s];
-        SOC10s = [SOC10s, data(BigIC(i)).SOC(56)];
-        R10s = [R10s, data(BigIC(i)).R10s];
-        SOC30s = [SOC30s, data(BigIC(i)).SOC(end)];
-        R30s = [R30s, data(BigIC(i)).R(end)];
+        SOC001sc = [SOC001sc, data(BigIC(i)).SOC(2)];
+        R001sc = [R001sc, data(BigIC(i)).R001s];
+        SOC1sc = [SOC1sc, data(BigIC(i)).SOC(11)];
+        R1sc = [R1sc, data(BigIC(i)).R1s];
+        SOC10sc = [SOC10sc, data(BigIC(i)).SOC(56)];
+        R10sc = [R10sc, data(BigIC(i)).R10s];
+        SOC30sc = [SOC30sc, data(BigIC(i)).SOC(end)];
+        R30sc = [R30sc, data(BigIC(i)).R(end)];
+    end
+    for i = 1 : length(BigID)
+        SOC001sd = [SOC001sd, data(BigID(i)).SOC(2)];
+        R001sd = [R001sd, data(BigID(i)).R001s];
+        SOC1sd = [SOC1sd, data(BigID(i)).SOC(11)];
+        R1sd = [R1sd, data(BigID(i)).R1s];
+        SOC10sd = [SOC10sd, data(BigID(i)).SOC(56)];
+        R10sd = [R10sd, data(BigID(i)).R10s];
+        SOC30sd = [SOC30sd, data(BigID(i)).SOC(end)];
+        R30sd = [R30sd, data(BigID(i)).R(end)];
     end
 elseif id_cfa == 3
     for i = 1:length(BigI)
-        SOC001s = [SOC001s, data(BigI(i)).SOC(2)];
+        SOC001sc = [SOC001sc, data(BigI(i)).SOC(2)];
         R001s = [R001s, data(BigI(i)).R001s];
         SOC1s = [SOC1s, data(BigI(i)).SOC(11)];
         R1s = [R1s, data(BigI(i)).R1s];
@@ -243,36 +270,66 @@ end
 %     end
 % end
 % spline을 사용하여 점들을 부드럽게 이어주기
-% Smoothed SOC-Resistance curve for SOC001s
-smoothed_SOC_001s = linspace(min(SOC001s), max(SOC001s), 100);
-smoothed_R_001s = spline(SOC001s, R001s, smoothed_SOC_001s);
+% Generate smoothed data for 'sc' case
+smoothed_SOC_001sc = linspace(min(SOC001sc), max(SOC001sc), 100);
+smoothed_R_001sc = spline(SOC001sc, R001sc, smoothed_SOC_001sc);
 
-smoothed_SOC_1s = linspace(min(SOC1s), max(SOC1s), 100);
-smoothed_R_1s = spline(SOC1s, R1s, smoothed_SOC_1s);
+smoothed_SOC_1sc = linspace(min(SOC1sc), max(SOC1sc), 100);
+smoothed_R_1sc = spline(SOC1sc, R1sc, smoothed_SOC_1sc);
 
-smoothed_SOC_10s = linspace(min(SOC10s), max(SOC10s), 100);
-smoothed_R_10s = spline(SOC10s, R10s, smoothed_SOC_10s);
+smoothed_SOC_10sc = linspace(min(SOC10sc), max(SOC10sc), 100);
+smoothed_R_10sc = spline(SOC10sc, R10sc, smoothed_SOC_10sc);
 
-smoothed_SOC_30s = linspace(min(SOC30s), max(SOC30s), 100);
-smoothed_R_30s = spline(SOC30s, R30s, smoothed_SOC_30s);
+smoothed_SOC_30sc = linspace(min(SOC30sc), max(SOC30sc), 100);
+smoothed_R_30sc = spline(SOC30sc, R30sc, smoothed_SOC_30sc);
 
-% 그래프 그리기
-figure;
+% Plot the graph for 'sc' case
+figure(1);
 hold on;
-plot(SOC001s, R001s, 'o');
-plot(smoothed_SOC_001s, smoothed_R_001s);
-plot(SOC1s, R1s, 'o');
-plot(smoothed_SOC_1s, smoothed_R_1s);
-plot(SOC10s, R10s, 'o');
-plot(smoothed_SOC_10s, smoothed_R_10s);
-plot(SOC30s, R30s, 'o');
-plot(smoothed_SOC_30s, smoothed_R_30s);
+plot(SOC001sc, R001sc, 'o');
+plot(smoothed_SOC_001sc, smoothed_R_001sc);
+plot(SOC1sc, R1sc, 'o');
+plot(smoothed_SOC_1sc, smoothed_R_1sc);
+plot(SOC10sc, R10sc, 'o');
+plot(smoothed_SOC_10sc, smoothed_R_10sc);
+plot(SOC30sc, R30sc, 'o');
+plot(smoothed_SOC_30sc, smoothed_R_30sc);
 hold off;
 
 xlabel('SOC');
-ylabel('Resistance (\Omega )', 'fontsize', 12);
-title('SOC vs Resistance');
-legend('100ms', '100ms (line)', '1s', '1s (line)', '10s', '10s (line)', '30s', '30s (line)' ,'Location', 'northwest'); 
-xlim([0 1])
+ylabel('Resistance (\Omega)', 'fontsize', 12);
+title('SOC vs Resistance (Charge)');
+legend('100ms', '100ms (line)', '1s', '1s (line)', '10s', '10s (line)', '30s', '30s (line)');
+xlim([0 1]);
 
+% Generate smoothed data for 'sd' case
+smoothed_SOC_001sd = linspace(min(SOC001sd), max(SOC001sd), 100);
+smoothed_R_001sd = spline(SOC001sd, R001sd, smoothed_SOC_001sd);
 
+smoothed_SOC_1sd = linspace(min(SOC1sd), max(SOC1sd), 100);
+smoothed_R_1sd = spline(SOC1sd, R1sd, smoothed_SOC_1sd);
+
+smoothed_SOC_10sd = linspace(min(SOC10sd), max(SOC10sd), 100);
+smoothed_R_10sd = spline(SOC10sd, R10sd, smoothed_SOC_10sd);
+
+smoothed_SOC_30sd = linspace(min(SOC30sd), max(SOC30sd), 100);
+smoothed_R_30sd = spline(SOC30sd, R30sd, smoothed_SOC_30sd);
+
+% Plot the graph for 'sd' case
+figure(2);
+hold on;
+plot(SOC001sd, R001sd, 'o');
+plot(smoothed_SOC_001sd, smoothed_R_001sd);
+plot(SOC1sd, R1sd, 'o');
+plot(smoothed_SOC_1sd, smoothed_R_1sd);
+plot(SOC10sd, R10sd, 'o');
+plot(smoothed_SOC_10sd, smoothed_R_10sd);
+plot(SOC30sd, R30sd, 'o');
+plot(smoothed_SOC_30sd, smoothed_R_30sd);
+hold off;
+
+xlabel('SOC');
+ylabel('Resistance (\Omega)', 'fontsize', 12);
+title('SOC vs Resistance (Discharge)');
+legend('100ms', '100ms (line)', '1s', '1s (line)', '10s', '10s (line)', '30s', '30s (line)');
+xlim([0 1]);
